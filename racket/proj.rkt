05ba09c773f518868159744ecd240fbf1d05c81a
Carlos Jimenez <carlosmjimenezg@gmail.com>
2023-11-23 18:21:15 -0400

    ...

--- racket/proj.rkt
+++ racket/proj.rkt
@@ -1,91 +1,75 @@
 #lang racket
 
-(display "Introduce el tamaño del tablero: ")
-(define uu (map string->number (string-split (read-line))))
+(define (is-valid board row col value N top bottom left right)
+  (let* ([row-values (vector->list (list-ref board row))] ; Usa vector->list aquí
+         [col-values (map (lambda (r) (vector-ref (list-ref board r) col)) (range N))]
+         [in-row? (member value row-values)]
+         [in-col? (member value col-values)])
+    (if (or in-row? in-col?)
+        #f
+        (let* ([right-seen (if (= col (- N 1))
+                               (let loop ([i (- N 2)] [max-height value] [seen 1])
+                                 (if (< i 0)
+                                     seen
+                                     (if (> (list-ref row-values i) max-height) ; Usa list-ref aquí
+                                         (loop (- i 1) (list-ref row-values i) (+ seen 1)) ; Usa list-ref aquí
+                                         (loop (- i 1) max-height seen))))
+                               0)]
+               [bottom-seen (if (= row (- N 1))
+                                (let loop ([i (- N 2)] [max-height value] [seen 1])
+                                  (if (< i 0)
+                                      seen
+                                      (if (> (vector-ref (list-ref board i) col) max-height)
+                                          (loop (- i 1) (vector-ref (list-ref board i) col) (+ seen 1))
+                                          (loop (- i 1) max-height seen))))
+                                0)])
+          (if (or (and (> right-seen 0) (not (= right-seen (list-ref right row)))) ; Usa list-ref aquí
+                  (and (> bottom-seen 0) (not (= bottom-seen (list-ref bottom col))))) ; Usa list-ref aquí
+              #f 
+              #t)))))
 
-(display "Introduce las pistas desde NORTE, separadas por espacios: ")
-(define top (map string->number (string-split (read-line))))
-
-(display "Introduce las pistas desde SUR, separadas por espacios: ")
-(define bottom (map string->number (string-split (read-line))))
-
-(display "Introduce las pistas desde la OESTE, separadas por espacios: ")
-(define left (map string->number (string-split (read-line))))
-
-(display "Introduce las pistas desde la ESTE, separadas por espacios: ")
-(define right (map string->number (string-split (read-line))))
-
-
-
-(define N 2)
-(define board (make-list N (make-list N 0)))
-
-
-(define (list-set lst idx val)
-  (for/list ([i (in-range (length lst))]
-             [v (in-list lst)])
-    (if (= i idx) val v)))
-
-
-
-(define (is_valid? row col value)
-  (let* ([fila-actual (list-ref board row)]
-         [columna-actual (for/list ([i (in-range N)]) (list-ref (list-ref board i) col))])
-    (not (or (member value fila-actual) (member value columna-actual))))
-  
-  (when (= col (- N 1)) ; Última columna, comprobar la pista de la derecha
-    (let* ([seen 1] ; El primer edificio siempre es visible
-           [max_height value] ; La altura máxima vista hasta ahora
-           [fila-actual (list-ref board row)])
-      (for ([i (in-range (- N 2) -1 -1)]) ; Recorrer la fila de derecha a izquierda
-        (when (> (list-ref fila-actual i) max_height) ; Si hay un edificio más alto
-            (begin
-              (set! seen (+ seen 1)) ; Aumentar el contador de edificios visibles
-              (set! max_height (list-ref fila-actual i))))) ; Actualizar la altura máxima
-      (when (not (= seen (list-ref right row))) ; Si el número de edificios visibles no coincide con la pista
-          #f ; El valor no es válido
-          #t)))
-
-  (when (= row (- N 1)) ; Última fila, comprobar la pista de abajo
-    (let* ([seen 1] ; El primer edificio siempre es visible
-           [max_height value] ; La altura máxima vista hasta ahora
-           [columna-actual (for/list ([i (in-range N)]) (list-ref (list-ref board i) col))])
-      (for ([i (in-range (- N 2) -1 -1)]) ; Recorrer la columna de abajo a arriba
-        (when (> (list-ref columna-actual i) max_height) ; Si hay un edificio más alto
-          (set! seen (+ seen 1)) ; Aumentar el contador de edificios visibles
-          (set! max_height (list-ref columna-actual i)))) ; Actualizar la altura máxima
-      (unless (= seen (list-ref bottom col)) ; Si el número de edificios visibles no coincide con la pista
-        #f))) ; El valor no es válido
-  #t) ; Si se pasa todas las comprobaciones, el valor es válido
-
-(define (solve row col)
-  ; Si se ha llegado al final del tablero, el puzzle está resuelto
-  (if (and (= row N) (= col 0))
-      #t
-      (if (= col N) ; Si se ha llegado al final de una fila, pasar a la siguiente
-          (solve (+ row 1) 0)
-          ; Probar todos los valores posibles de 1 a N
+(define (solve board row col N top bottom left right)
+  (if (= row N)
+      (begin (display "Solution:\n") (for-each (lambda (r) (display r) (newline)) board))
+      (if (= col N)
+          (solve board (+ row 1) 0 N top bottom left right)
           (let loop ([value 1])
-            (cond
-              [(> value N) #f] ; Si ninguno de los valores funciona, el puzzle no tiene solución
-              [(is_valid? row col value)
-               (begin
-                 ; Colocar el valor en el tablero
-                 (list-set (list-ref board row) col value)
-                 ; Intentar resolver el resto del tablero
-                 (or (solve row (+ col 1))
-                     ; Si no se puede resolver, deshacer el cambio y probar otro valor
-                     (begin
-                       (list-set (list-ref board row) col 0)
-                       (loop (+ value 1)))))]
-              [else (loop (+ value 1))])))))
-
- (if (solve 0 0)
-    (begin
-      (display "Solución:\n")
-      (for ([row board])
-        (for-each (lambda (value) (printf "~a " value)) row)
-        (newline)))
-    (display "No hay solución\n"))
+            (if (> value N)
+                #f
+                (if (is-valid board row col value N top bottom left right)
+                    (begin
+                      (vector-set! (list-ref board row) col value) ; Usa list-ref aquí
+                      (if (solve board row (+ col 1) N top bottom left right)
+                          #t
+                          (begin
+                            (vector-set! (list-ref board row) col 0) ; Usa list-ref aquí
+                            (loop (+ value 1)))))
+                    (loop (+ value 1))))))))
 
 
+(define N 4) ; Define N aquí tamaño del tablero
+
+
+; Define todas las vistas
+(define (main)
+  ;(display "Enter the size of the board: ")
+   (let* ([board (for/list ([i N]) (make-vector N 0))])
+    
+    (display "Introduce las vistas del NORTE, SEPARADA POR ESPACIOS ")
+     (let* ([top (map string->number (string-split (read-line)))]
+           [top-len (length top)])
+       
+      (display "Introduce las vistas del SUR, SEPARADA POR ESPACIOS ")
+       (let* ([bottom (map string->number (string-split (read-line)))]
+           [bottom-len (length bottom)])
+        (display "Introduce las vistas del OESTE, SEPARADA POR ESPACIOS ")
+         (let* ([left (map string->number (string-split (read-line)))]
+           [left-len (length left)])
+          (display "Introduce las vistas del ESTE, SEPARADA POR ESPACIOS ")
+           (let* ([right (map string->number (string-split (read-line)))]
+           [right-len (length right)])
+            (if (and (= top-len N) (= bottom-len N) (= left-len N) (= right-len N))
+                (solve board 0 0 N top bottom left right)
+                (display "Entrada invalida. Introduzco mas vistas que el rango del tablero"))))))))
+
+(main)
